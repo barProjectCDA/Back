477de8a3fb052bc0efa67c1aa46ab48ff9178746
= <colonnact@outlook.com>
2025-03-11 21:34:55 +0100

    coorection extra

--- src/main/java/fr/cactus/api/models/Product.java
+++ src/main/java/fr/cactus/api/models/Product.java
@@ -1,7 +1,10 @@
 package fr.cactus.api.models;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +12,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -38,12 +43,19 @@ public class Product {
     @Column(name = "\"css_hexadecimal_color\"")
     private String cssHexadecimalColor;
 
+
     @ManyToOne
     @JsonIgnore
     @JsonBackReference
     @JoinColumn(name = "id_category", nullable = false)
     private Category category;
 
-
-
+    @ManyToMany
+    @JsonManagedReference
+    @JoinTable(
+        name = "product_extra", 
+        joinColumns = @JoinColumn(name = "id_product"), 
+        inverseJoinColumns = @JoinColumn(name = "id_extra")
+    )
+    private List<Extra> extras;
 }
